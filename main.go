36e8d2bd3abebdc7de11015f6c0b07c65132adce
Kasper Olsen <kasper@kols.dk>
2022-05-04 15:36:19 +0200

    fixed bug about min not being set and refact code

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"math"
 	"net/http"
 	"os"
 	"time"
@@ -57,42 +58,61 @@ type IndexViewModel struct {
 	WeatherData []WeatherResponseData
 }
 
-func UnmarshalWelcome(data []byte) (WeatherObservation, error) {
+func UnmarshalWeatherObservation(data []byte) (WeatherObservation, error) {
 	var r WeatherObservation
 	err := json.Unmarshal(data, &r)
 	return r, err
 }
 
-func getApiKeyFromEnvVariable() string {
-	envVar := os.Getenv("DMI_API_KEY")
-	if envVar == "" {
-		panic("DMI_API_KEY is not set")
+func getEnvVariable(name string) string {
+	value := os.Getenv(name)
+	if value == "" {
+		panic(fmt.Sprintf("Environment variable %s is not set", name))
 	}
-	return envVar
+	return value
 }
 
 func formatDate(date time.Time) string {
 	return date.Format("2006-01-02T15:04:05Z")
 }
 
-func getWatherObservations(from, to time.Time) WeatherObservation {
-	uri := fmt.Sprintf("https://dmigw.govcloud.dk/v2/metObs/collections/observation/items?datetime=%s/%s&stationId=06186&parameterId=temp_dry&bbox-crs=https://www.opengis.net/def/crs/OGC/1.3/CRS84&api-key=%s", formatDate(from), formatDate(to), getApiKeyFromEnvVariable())
+func generateUri(fromDate, toDate time.Time) string {
+	uri := fmt.Sprintf(
+		"https://dmigw.govcloud.dk/v2/metObs/collections/observation/items?datetime=%s/%s&stationId=06186&parameterId=temp_dry&bbox-crs=https://www.opengis.net/def/crs/OGC/1.3/CRS84&api-key=%s",
+		formatDate(fromDate),
+		formatDate(toDate),
+		getEnvVariable("DMI_API_KEY"),
+	)
+	return uri
+}
 
+func buildRequest(uri string) *http.Request {
 	req, _ := http.NewRequest("GET", uri, nil)
 	req.Header.Add("accept", "application/geo+json")
+	return req
+}
+
+func doRequest(request *http.Request) *http.Response {
 	client := &http.Client{}
-	resp, err := client.Do(req)
+	resp, err := client.Do(request)
 
 	if err != nil {
 		panic(err)
 	}
 
-	body, err := ioutil.ReadAll(resp.Body)
+	return resp
+}
+
+func getWatherObservations(from, to time.Time) WeatherObservation {
+	uri := generateUri(from, to)
+	request := buildRequest(uri)
+	response := doRequest(request)
+	body, err := ioutil.ReadAll(response.Body)
 	if err != nil {
 		panic(err)
 	}
 
-	weatherObs, err := UnmarshalWelcome(body)
+	weatherObs, err := UnmarshalWeatherObservation(body)
 
 	if err != nil {
 		panic(err)
@@ -101,7 +121,8 @@ func getWatherObservations(from, to time.Time) WeatherObservation {
 }
 
 func getMinAndMax(features []Feature) (float64, float64) {
-	var min, max float64
+	min := math.Inf(1)
+	max := math.Inf(-1)
 	for _, feature := range features {
 		if feature.Properties.Value < min {
 			min = feature.Properties.Value
@@ -113,29 +134,33 @@ func getMinAndMax(features []Feature) (float64, float64) {
 	return min, max
 }
 
+func getIndex(c *gin.Context) {
+	viewModel := IndexViewModel{
+		Date:        time.Now().Format("January 02"),
+		WeatherData: []WeatherResponseData{},
+	}
+	for i := 1; i <= 10; i++ {
+		year := time.Now().Year() - i
+		month := time.Now().Month()
+		day := time.Now().Day()
+		fromDate := time.Date(year, month, day, 0, 0, 0, 0, time.Now().Location())
+		toDate := time.Date(year, month, day, 23, 59, 0, 0, time.Now().Location())
+		w := getWatherObservations(fromDate, toDate)
+		min, max := getMinAndMax(w.Features)
+		obs := WeatherResponseData{Year: year, Min: min, Max: max}
+		viewModel.WeatherData = append(viewModel.WeatherData, obs)
+	}
+
+	c.HTML(http.StatusOK, "index.tmpl", gin.H{
+		"data": viewModel,
+	})
+}
+
 func main() {
 	router := gin.Default()
 	router.LoadHTMLGlob("templates/*")
 	router.Static("/assets", "./assets")
 
-	router.GET("/", func(c *gin.Context) {
-		viewModel := IndexViewModel{
-			Date:        time.Now().Format("January 02"),
-			WeatherData: []WeatherResponseData{},
-		}
-		for i := 1; i <= 10; i++ {
-			year := time.Now().Year() - i
-			month := time.Now().Month()
-			day := time.Now().Day()
-			w := getWatherObservations(time.Date(year, month, day, 0, 0, 0, 0, time.Now().Location()), time.Date(year, month, day, 23, 59, 0, 0, time.Now().Location()))
-			min, max := getMinAndMax(w.Features)
-			obs := WeatherResponseData{Year: year, Min: min, Max: max}
-			viewModel.WeatherData = append(viewModel.WeatherData, obs)
-		}
-
-		c.HTML(http.StatusOK, "index.tmpl", gin.H{
-			"data": viewModel,
-		})
-	})
+	router.GET("/", getIndex)
 	router.Run(":8080")
 }
