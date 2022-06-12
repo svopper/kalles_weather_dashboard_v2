185d21220c829853e0af38c6a9e1d610d66d5758
Kasper Olsen <kasper@kols.dk>
2022-06-12 15:28:24 +0200

    changed init

--- app/main.go
+++ app/main.go
@@ -1,8 +1,19 @@
 package main
 
-import "github.com/svopper/kalles_weather_dashboard_v2/app/server"
+import (
+	"log"
+	"os"
+
+	"github.com/svopper/kalles_weather_dashboard_v2/app/server"
+)
 
 func main() {
 	router := server.InstantiateControllers()
-	router.Run(":8080")
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Println("PORT environment variable not set. Defaulting to 8080")
+		port = "8080"
+	}
+	router.Run(":" + port)
 }
